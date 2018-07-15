ec3e3f5510f0ffd776b74f7f14da11307db87a0e
sadness_ojisan <sadness.ojisan@gmail.com>
2018-07-15 17:34:42 +0900

    [add] NininbaoriViewController.swift

---
+++ coach-in-app/NininbaoriViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NininbaoriViewController.swift
+//  coach-in-app
+//
+//  Created by ide yuta on 2018/07/15.
+//  Copyright © 2018年 ojisan. All rights reserved.
+//
+
+import UIKit
+
+
+class NininbaoriViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+}
+
