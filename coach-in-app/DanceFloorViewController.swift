01c8fc880150f95d6d2a8c9612d69bdef331cea9
sadness_ojisan <sadness.ojisan@gmail.com>
2018-07-15 18:58:19 +0900

    [update] can transition to dancefloor view

---
+++ coach-in-app/DanceFloorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DanceFloorViewController.swift
+//  coach-in-app
+//
+//  Created by ide yuta on 2018/07/15.
+//  Copyright © 2018年 ojisan. All rights reserved.
+//
+
+import UIKit
+
+class DanceFloorViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+}
+
+
