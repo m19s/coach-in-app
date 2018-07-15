77edfa1f95536dfbd831a111398232577dd27fad
sadness_ojisan <sadness.ojisan@gmail.com>
2018-07-15 17:16:43 +0900

    [update] transition to nininbaori

--- coach-in-app/ViewController.swift
+++ coach-in-app/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func handleClickNininbaoriBtn(_ sender: UIButton) {
+        let storyboard: UIStoryboard = self.storyboard!
+        let nininbaori = storyboard.instantiateViewController(withIdentifier: "nininbaori")
+        self.present(nininbaori, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 
 
